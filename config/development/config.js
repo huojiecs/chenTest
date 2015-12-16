22b9f51351a9d3d4232090572f76d13474162ebd
chen001 <125569489@qq.com>
2015-12-16 11:39:03 +0800

    dbTest

--- config/development/config.js
+++ config/development/config.js
@@ -3,11 +3,11 @@
  */
 var config = {
     dbUrl:'mongodb://localhost/chenTest',
-    dbHost: '10.9.58.169',
+    dbHost: '127.0.0.1',
     dbPost: '27017',
-    dbBase: 'b135152d-4b7c-4e9f-8d9f-eafe6408d67c',
-    dbUser: '2c04b3c7-e1e7-459c-a111-236ceab25564',
-    dbPass: 'kYqXZTYdMtP-ZR5GsUuH7Q'
+    dbBase: 'chenTest',
+    dbUser: '',
+    dbPass: ''
 };
 //mongodb://localhost/chenTest
 //127.0.0.1
--- routes/index.js
+++ routes/index.js
@@ -2,8 +2,8 @@ var express = require('express');
 var router = express.Router();
 var mongoose = require('mongoose');
 var user = require('../models/user').user;
-var config = require('../config/config');
-
+//var config = require('../config/development/config');   //测试环境
+var config = require('../config/config');           //生产环境
 /** session 配置部分 开始  */
 var session = require('express-session');
 var MongoStore = require('connect-mongo')(session);
@@ -12,18 +12,28 @@ var setting = require('.././setting');
 
 //'mongodb://huojiecs:chenshuo89@ds031883.mongolab.com:31883/chentest'
 //mongoose.connect('mongodb://huojiecs:chenshuo89@ds031883.mongolab.com:31883/chentest');//:31883/chentest
-
-mongoose.connect(config.dbUrl);
+//mongodb://d431d25b-7a88-4251-b061-d902b9d30e14:T5CLMWu_LHZxTdxOeNgzSA@10.9.58.169:27017/b135152d-4b7c-4e9f-8d9f-eafe6408d67c
+//mongoose.connect(config.dbUrl);
+mongoose.connect(config.dbHost+":"+config.dbPost+"/"+config.dbBase, {user: config.dbUser, pass: config.dbPass});
 
 router.use(session({
     secret: setting.cookieSecret,
     cookie: {maxAge: 80000 },
     store: new MongoStore({
-        host: config.dbHost,    //数据库的地址，本机的话就是127.0.0.1，也可以是网络主机
-        port: config.dbPost,          //数据库的端口号 27017
+        host: config.dbHost,   //数据库的地址，本机的话就是127.0.0.1，也可以是网络主机
+        port: 27017,          //数据库的端口号 27017
         db : setting.db
     })
 }));
+//router.use(session({
+//    secret: setting.cookieSecret,
+//    cookie: {maxAge: 80000 },
+//    store: new MongoStore({
+//        host: config.dbHost,    //数据库的地址，本机的话就是127.0.0.1，也可以是网络主机
+//        port: config.dbPost,          //数据库的端口号 27017
+//        db : setting.db
+//    })
+//}));
 /** session 配置部分 结束  */
 
 /* GET home page. */
